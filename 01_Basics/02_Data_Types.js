ccb80ad631f4a7f6a8813864b9a23bbe88125215
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-02-19 10:50:33 +0000

    Data Types

---
+++ 01_Basics/02_Data_Types.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be higher
+
+console.log("Samar")
+
+let name = "Samar"
+let age = 16
+let isLoggedIn = true
+let state;
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
