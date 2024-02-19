6d708d05edbbeecfdb6212f0fc5153ad11c51394
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-02-19 11:26:26 +0000

    conversion of data types

---
+++ 01_Basics/03_Conversion_Operations.js
@@ -0,0 +1,26 @@
+let score = "Samar"
+
+// console.log(typeof(score));
+
+let scoreInNumber = Number(score)
+// console.log(typeof(scoreInNumber));
+// console.log(scoreInNumber);
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = "samar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "samar" => true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
