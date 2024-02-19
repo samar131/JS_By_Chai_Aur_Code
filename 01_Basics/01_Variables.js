1b99e30e75eb000bc46e31681fa9f909549fcc1b
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-02-19 05:58:17 +0000

    Variables file done!

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "samarverma@gamil.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "sv@gmail.com"
+accountPassword = "12121212"
+accountCity = "Bengaluru"
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
