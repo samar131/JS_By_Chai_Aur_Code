d868fdf70293798e10611d94a5187f65853aa32d
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-02-20 10:46:37 +0000

    Js operations done!

--- 01_Basics/03_Conversion_Operations.js
+++ 01_Basics/03_Conversion_Operations.js
@@ -14,7 +14,7 @@ let scoreInNumber = Number(score)
 let isLoggedIn = "samar"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -23,4 +23,32 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33 
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+//****************** Operations************************/
+ 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello, "
+let str2 = "Samar"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
