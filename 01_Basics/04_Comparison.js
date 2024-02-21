777a2743e1e37239e2f2c03291778b6945fdea78
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-02-21 08:43:02 +0000

    comparisons

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//********************************************************** */
+// console.log("2" > 0)
+// console.log("02" > 0)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+  // avoid thes type of comparison
+//********************************************************** */
+
+// strict check ===
+console.log("2" == 2)
+console.log("2" === 2)// === also checks data type
