2a37b840efa3401d7dd3745a3777114e3026b11b
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-02-22 04:47:46 +0000

    05 file name changed

---
+++ 01_Basics/05_Stack&Heap.js
@@ -0,0 +1,19 @@
+// Primitive 
+// 7 Types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+
+
+// Reference Type(Non - Primitive)
+// Array, Objects, Functions
+
+
+
+//******************************************************************* */
+
+// Stack (Primitive), Heap(Non-Primitive)
+let name1 = "Samar";
+
+let anotherName = name1;
+
