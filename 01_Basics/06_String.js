8c2f836f32ea5b63efba5d450eabc729bb041b2b
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-02-23 12:39:39 +0000

    prototypes

--- 01_Basics/06_String.js
+++ 01_Basics/06_String.js
@@ -1,4 +1,9 @@
 const name = "Samar"
 const age = 16
 
-console.log(name + age + );
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const gameName = new String("SamarV")
+
+console.log(gameName[0])
+console.log(gameName.__proto__)
