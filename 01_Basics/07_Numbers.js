1709d3e14f3518062e1ea23b5d2a11ca29a93b58
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-02-27 06:26:59 +0000

    Numbers

---
+++ 01_Basics/07_Numbers.js
@@ -0,0 +1,23 @@
+const score = 400 
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const nums = 123.8955
+
+// console.log(nums.toPrecision(4))
+
+const number = 1000000
+// console.log(number.toLocaleString("en-IN"))
+
+//+++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.7))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.2))
