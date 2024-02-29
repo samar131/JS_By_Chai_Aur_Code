5ac13e06395b7f7f81936aeb96a20838e0ab5a61
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-02-29 06:57:02 +0000

    date

--- 01_Basics/08_Date_Time.js
+++ 01_Basics/08_Date_Time.js
@@ -1,1 +1,17 @@
-console.log("samar")
+// Date
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2007, 4, 15)
+let myCreatedDate = new Date(2007, 4, 15, 5, 3)
+// console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
