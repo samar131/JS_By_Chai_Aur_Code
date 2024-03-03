b6afac0a6156fea365d00fef906a4bf1b49badd6
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-03-03 06:51:37 +0000

    arrays

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -1,1 +1,40 @@
 // array
+
+const heroes = ['batman', 'spiderman']
+const myArr = [1, 2, 3, 4,]
+
+const newArr = new Array(0, 1, 2, 3, 4, 5)
+
+// console.log(newArr[1]);
+
+// array methods
+
+// newArr.push(6);
+// newArr.push(7);
+// newArr.pop()
+
+// newArr.unshift(9)
+// newArr.shift()
+
+// console.log(newArr.includes(9))
+// console.log(newArr.indexOf(3))
+
+// const array = newArr.join()
+
+// console.log(newArr);
+// console.log(array);
+
+
+// slice and splice
+console.log("A ", newArr)
+
+const my1 = newArr.slice(1, 3)
+
+console.log(my1)
+console.log("B ", newArr)
+
+
+
+const my2 = newArr.splice(1, 3)
+console.log("C ", newArr)
+console.log(my2)
