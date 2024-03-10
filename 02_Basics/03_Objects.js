599166d7e894aa0926d4fd3c5fa713ed7109dd96
Samar Verma <125805268+samar131@users.noreply.github.com>
2024-03-10 10:19:35 +0000

    objects

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,11 @@
+// object literals
+
+const JsUser = {
+    name: "Samar",
+    age: 18,
+    location: "Delhi",
+    email: "samar@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Mondya", "Satuerday"]
+}
+
